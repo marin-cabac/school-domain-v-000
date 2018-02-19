8e7dcc3e720485469ba3fea1c65094b35f04a5fb
Marin Cabac <marin.cabac@gmail.com>
2018-02-19 14:37:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,20 +2,21 @@
 
 class School
   attr_accessor :school, :roster
-roster={}
+  roster={}
+######################################
   def initialize(school_n)
     @school=school_n
     @roster = {}
      end
-
+######################################
 def add_student(n,g)
  @roster[g] ||= []
   @roster[g]<<n
 end
-
+######################################
 def grade(g)
   @roster[g]
-
 end
+######################################
 
 end
